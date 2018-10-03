17e60531d0b221d5ee4a78441952b88c72b1fac1
itggot-erik-landmark <eriklandmark@DESKTOP-K1GD9GC.localdomain>
2018-10-03 17:36:42 +0200

    Fixed Routing and layout

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
-  div
-    md-tabs(md-sync-route)
-      md-tab(id="tab-calendar" md-label="Calendar" to="/components/TabsTest.vue")
-      md-tab(id="tab-reserve" md-label="Reserve") 
-      md-tab(id="tab-modify" md-label="Change") 
+
+    div
+        md-tabs(md-sync-route)
+            md-tab(id="tab-stora" md-label="Stora Konferens" to="/stora")
+            md-tab(id="tab-lilla" md-label="Lilla Konferens" to="/lilla")
+            md-tab(id="tab-boka" md-label="Boka" to="/boka")
 
 
 </template>
@@ -17,12 +18,9 @@ export default Vue.extend({
 <style lang="scss" scoped>
 
   .md-tabs{
-    
     height: 100%;
     width: 100%;
-    }
-  
-  
+  }
   
 </style>
 
